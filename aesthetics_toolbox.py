05ab86bbdda35cc59fa812b02f28f9b0026af45e
Ralf Bartho <35026987+RBartho@users.noreply.github.com>
2024-04-25 11:48:40 +0200

    Update aesthetics_toolbox.py

--- aesthetics_toolbox.py
+++ aesthetics_toolbox.py
@@ -62,36 +62,6 @@ Image.MAX_IMAGE_PIXELS = 1e10
 
 
 
-def select_folder():
-    root = tk.Tk()
-    root.withdraw()
-    folder_path = filedialog.askdirectory(master=root)
-    root.destroy()
-    return folder_path
-
-
-# import base64
-
-# @st.cache_data()
-# def get_base64_of_bin_file(bin_file):
-#     with open(bin_file, 'rb') as f:
-#         data = f.read()
-#     return base64.b64encode(data).decode()
-
-# def set_png_as_page_bg(png_file):
-#     bin_str = get_base64_of_bin_file(png_file)
-#     page_bg_img = '''
-#     <style>
-#     .stApp {
-#     background-image: url("data:image/png;base64,%s");
-#     background-size: cover;
-#     }
-#     </style>
-#     ''' % bin_str
-    
-#     st.markdown(page_bg_img, unsafe_allow_html=True)
-#     return
-
 
 def custom_round(num):
     '''
