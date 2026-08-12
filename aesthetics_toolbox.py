a7fcbee6981677296736786fdb20491123171d04
Ralf Bartho <35026987+RBartho@users.noreply.github.com>
2026-08-12 11:25:55 +0200

    Update aesthetics_toolbox.py
    removed duplicate line

--- aesthetics_toolbox.py
+++ aesthetics_toolbox.py
@@ -82,7 +82,7 @@ st.write('')
 st.markdown('<p class="contr">Contact and GitHub</p>', unsafe_allow_html=True)
 st.markdown('Questions, suggestions, bugs: ralf.bartho@gmail.com', unsafe_allow_html=True)
 st.markdown('GitHub repository: https://github.com/RBartho/Aesthetics-Toolbox', unsafe_allow_html=True)
-st.markdown('GitHub repository: https://github.com/RBartho/Aesthetics-Toolbox', unsafe_allow_html=True)
+
 
 
     
